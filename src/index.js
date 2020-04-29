ee39f9781252ff342093ffb96cbabf0f73257054
vitcool@ukr.net <vitcool@ukr.net>
2020-04-29 12:33:47 +0300

    Simple counter based on websockets is added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const path = require("path");
-const http = require('http');
+const http = require("http");
 const express = require("express");
-const socketio = require('socket.io');
-
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
@@ -17,6 +16,17 @@ server.listen(port, () => {
   console.log(`Server is up and runnig on port ${port}`);
 });
 
-io.on('connection', () => {
-  console.log('new WebSocket connection');
-})
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("new WebSocket connection");
+
+  socket.emit('countUpdated', count);
+
+  socket.on("increment", () => {
+    count += 1;
+    console.log(`the count has been updated - ${count}`);
+    // socket.emit('countUpdated', count); - emit event just for particular client
+    io.emit('countUpdated', count);
+  });
+});
