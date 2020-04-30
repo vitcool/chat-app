e0da03dd07b7de4f382a52bd968fff403bc08a5e
vitcool@ukr.net <vitcool@ukr.net>
2020-04-30 10:37:32 +0300

    Event acknowledgments are added

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,10 +5,36 @@ socket.on("message", (message) => {
 });
 
 const messageForm = document.querySelector("#message-form");
+const sendLocation = document.querySelector("#send-location");
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const { message } = e.target.elementsß;
+  const { message } = e.target.elements;
   const messageText = message.value;
-  socket.emit("sendMessage", messageText);
+  socket.emit("sendMessage", messageText, (error) => {
+    if (error) {
+      return console.log(`Received the following error - ${error}`);
+    }
+    console.log("Message delivered");
+  });
+});
+
+sendLocation.addEventListener("click", () => {
+  const { geolocation } = navigator;
+  if (!geolocation) {
+    return alert("Geoloaction is not supported by your browser");
+  }
+
+  geolocation.getCurrentPosition(
+    (pos) => {
+      const { coords } = pos;
+      const { latitude, longitude } = coords;
+      const obj = { latitude, longitude };
+      socket.emit("sendLocation", obj, () => {
+        console.log("Location shared!");
+      });
+    },
+    () => console.log("HUI"),
+    { timeout: 10000 }
+  );
 });
