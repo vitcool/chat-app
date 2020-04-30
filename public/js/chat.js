6014fa0d85327921424b5c0144b4b5ca1abc1ee2
vitcool@ukr.net <vitcool@ukr.net>
2020-04-30 11:14:18 +0300

    Message template is added

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,9 +5,15 @@ const $messageForm = document.querySelector("#message-form");
 const $messageFormInput = $messageForm.querySelector('input');
 const $messageFormButton = $messageForm.querySelector('button');
 const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector('#messages');
+
+//templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
 
 socket.on("message", (message) => {
   console.log(`New message received - ${message}`);
+  const html = Mustache.render(messageTemplate, { message });
+  $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
